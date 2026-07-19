57c212e96a1586d8b0297cfe06b092879498e06e
rickysurya <rickysuryaa10@gmail.com>
2026-07-19 22:57:05 +0700

    add leetcode.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,17 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  cmd = "Leet",
+  dependencies = {
+    -- include a picker of your choice, see picker section for more details
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    -- configuration goes here
+    lang = "golang",
+    storage = {
+      home = "/home/ricky/Git/leetcode-go", -- Main Repo
+    },
+  },
+}
