549909453443429246abc83349e815f7ab768503
rickysurya <rickysuryaa10@gmail.com>
2026-07-29 22:59:50 +0700

    feat(nvim): revert back from everforest to catppuccin

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "catppuccin-mocha",
       -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
       highlights = {
         init = {},
